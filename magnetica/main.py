554588a52097235b3f7313d7e89e16e679cd54f9
Hazy2006 <alexandruioan.hathazi@gmail.com>
2026-08-08 12:55:45 +0300

    Added size/strength formula

--- magnetica/main.py
+++ magnetica/main.py
@@ -10,8 +10,16 @@ def main():
     x, y = np.meshgrid(xs, ys)
 
     magnets = [
-        Magnet(position=(0, 0), moment=(0, 1))   # one magnet at origin, pointing +y
+        Magnet(position=(0, 0), moment=(0, 1)),
+        Magnet(position=(1, -1), moment=(3, 3)),
+        Magnet(position=(3, -1), moment=(-2, 0)),
     ]
+
+    for magnet in magnets:
+        strength = np.sqrt(magnet.moment[0] ** 2 + magnet.moment[1] ** 2)
+        size = 50 + strength * 40  # base size 50, grows with strength
+        plt.scatter(magnet.position[0], magnet.position[1],
+                    c='red', s=size, edgecolors='black', zorder=5)
 
     bx, by = compute_total_field(magnets, x, y)
     magnitude = np.sqrt(bx ** 2 + by ** 2)
